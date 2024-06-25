09f65175d1d0a675ebfa9742d78d8b5eb51270d7
Muxsinjon1016 <muxsinjon1016@gmail.com>
2024-06-25 13:09:36 +0500

    finnnn

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-blue-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem cumque
+        ipsum praesentium fugiat aliquam. Qui aliquam natus optio. Aspernatur
+        beatae placeat culpa! Minus quam adipisci similique, placeat nam
+        voluptate quaerat?
+      </p>
+    </>
+  );
+}
+
+export default App;
